7a155e31c22a58daede08c1bd69f017fe2157721
AlanJager <873863981@qq.com>
2017-01-01 01:13:19 +0800

    update code style

--- Sina_spider/pipelines.py
+++ Sina_spider/pipelines.py
@@ -6,22 +6,21 @@ from items import InformationItem, TweetsItem, FollowsItem, FansItem
 class MongoDBPipleline(object):
     def __init__(self):
         self.init(pymongo.MongoClient("localhost", 27017)["weibos"])
-        
 
     def init(self, db):
         self.tables = {
-            "information" : db["Information"],
-            "tweets" : db["Tweets"],
-            "follows" : db["Follows"],
-            "fans" : db["Fans"]        
+            "information": db["Information"],
+            "tweets": db["Tweets"],
+            "follows": db["Follows"],
+            "fans": db["Fans"]        
         }
 
     # 插入多条
     def inserts(self, item, table):
         items = dict(item)
         rows = items.pop(table)
-        for i in range(len(rows)):
-            items[str(i + 1)] = rows[i]
+        for index, value in enumerate(rows)
+            items[str(index + 1)] = value
         try:
             self.Follows.insert(items)
         except Exception:
